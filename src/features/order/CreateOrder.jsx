b2e62c53e834255e5d62f0a64aec01ab0ac38718
Alazar Bedada <lazarusbedada@gmail.com>
2026-04-29 12:49:40 +0300

    by using the redux and the thunk i make the page more good than yesterday

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,51 +1,40 @@
 
-import { Form, useNavigation } from "react-router-dom";
+import { Form, redirect, useNavigation } from "react-router-dom";
 import Button from "../../ui/Button";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart, getCart, getTotalCartPrice } from "../cart/cartSlice";
+import EmptyCart from "../cart/EmptyCart";
+import store from "../../store";
+import { createOrder } from "../../services/apiRestaurant";
+import { formatCurrency } from "../../utils/helpers";
+import { useState } from "react";
+import { fetchAddress } from "../user/userSlice";
 
 // https://uibakery.io/regex-library/phone-number
-// const isValidPhone = (str) =>
-//   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-//     str
-//   );
+const isValidPhone = (str) =>
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(str );
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+
 
 function CreateOrder() {
     const username=useSelector(state=>state.user.username);
+  // const formErrors=useActionData();
+  const dispatch=useDispatch()
 
-
-  // const [withPriority, setWithPriority] = useState(false);
+  const [withPriority, setWithPriority] = useState(false);
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
-  const cart = fakeCart;
+  const cart = useSelector(getCart);
+
+  const totalCartPrice=useSelector(getTotalCartPrice)
+  const priorityPrice=withPriority ? totalCartPrice * 0.20 : 0
+  // const totalPrice=totalCartPrice+priorityPrice
+  if(!cart.length) return <EmptyCart />
 
   return (
     <div className="px-4 py-6">
       <h2 className="text-xl font-semibold mb-8">Ready to order? Let's go!</h2>
-
+      <button onClick={()=>dispatch(fetchAddress())} >Get Position</button>
       {/* <Form method="POST" action="/order/new"> */}
       <Form method="POST">
         <div className="mb-5 flex  flex-col gap-2 sm:flex-row sm:items-center ">
@@ -82,8 +71,8 @@ function CreateOrder() {
             type="checkbox"
             name="priority"
             id="priority"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+            value={withPriority}
+            onChange={(e) => setWithPriority(e.target.checked)}
           />
           <label htmlFor="priority" className="font-medium">Want to yo give your order priority?</label>
         </div>
@@ -91,18 +80,31 @@ function CreateOrder() {
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)}/>
           <Button disabled={isSubmitting}  type="primary">
-                {isSubmitting ? "Placing order..." : "Order now"}
-                </Button>
+                {isSubmitting ? "Placing order..." : `Order now from ${formatCurrency(totalCartPrice)}`}
+                </Button>  
         </div>
       </Form>
     </div>
   );
 }
 export async function action({request}){
-  // const formData = request.formData()
-  // const data = Object.fromEntries(formData)
+  const formData = await request.formData()
+  const data = Object.fromEntries(formData)
 
-  return null
+  const order={
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === "true"
+  }
+ const errors={}
+ if(!isValidPhone(order.phone))
+   errors.phone="Please give us  your correct phone number, we might need it to contact you"
+  if(Object.keys(errors).length>0) return errors
+
+ const newOrder=await createOrder(order)
+
+ store.dispatch(clearCart())
+  return redirect(`/order/${newOrder.id}`)
 }
 
 export default CreateOrder;
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 function Button({children,to,disabled,type,onClick}) {
-const base=" bg-yellow-400 text-sm  font-semibold uppercase  inline-block rounded-full text-stone-800 tracking-wide hover:bg-yellow-300  transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:bg-yellow-300 disabled:cursor-not-allowed ";
+const base=" bg-yellow-400 text-sm  font-bold uppercase  inline-block rounded-full text-stone-800 tracking-wide hover:bg-yellow-300  transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:bg-yellow-300 disabled:cursor-not-allowed ";
 const styles={
    primary: base + " px-4 py-3 md:px-6 md:py-4 ",
    small: base + "px-4 py-2 md:px-5 md:py-2.5 text-xs ",
+   round: base + "px-2.5 py-1 md:px-3.5 md:py-2 text-sm ",
    secondary: " bg-transparent text-sm border-2 border-stone-300  font-semibold uppercase  inline-block rounded-full text-stone-400 hover:text-stone-800 focus:text-stone-800  tracking-wide hover:bg-stone-300  transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 focus:bg-stone-200 disabled:cursor-not-allowed  px-4 py-2.5 md:px-6 md:py-3.5 "
 }
   if(to)
